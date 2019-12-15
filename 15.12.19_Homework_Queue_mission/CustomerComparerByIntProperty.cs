2a4f52de23e92021d3de899c48d54e6513fef374
GilMuzika <47449752+GilMuzika@users.noreply.github.com>
2019-12-16 01:05:37 +0200

    Add files via upload

---
+++ 15.12.19_Homework_Queue_mission/CustomerComparerByIntProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._12._19_Homework_Queue_mission
+{
+    class CustomerComparerByIntProperty<T> : IComparer<T>
+    {
+        public string PropertyName { get; set; } = "xxx";
+        public int Compare(T x, T y)
+        {
+            int propertyX = (int)x.GetType().GetProperties().Where(n => n.Name.Equals(PropertyName)).First().GetValue(x);
+            int propertyY = (int)y.GetType().GetProperties().Where(n => n.Name.Equals(PropertyName)).First().GetValue(y);
+
+            return propertyX - propertyY;
+        }
+    }
+}
